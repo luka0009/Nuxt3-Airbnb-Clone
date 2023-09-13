0a1bd53b7d666591bc8f227f2fa63dd76a766ea6
luka <lukabarbakadze18@gmail.com>
2023-09-13 22:46:04 +0400

    project setup

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>Good luck, Luka!</div>
+</template>
+
+<script setup lang="ts">
+	import { prisma } from "../prisma/db";
+
+	// const getUsers = async () => {
+	// 	const users = await prisma.user.findMany({});
+
+	// 	console.log(users);
+	// };
+
+	// getUsers();
+</script>
+
+<style scoped></style>
