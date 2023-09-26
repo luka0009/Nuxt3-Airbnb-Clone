13fecbf13bda55c2f10e71bb4d67694643eea48a
luka <lukabarbakadze18@gmail.com>
2023-09-26 12:32:21 +0400

    refactoring

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,9 @@
 	<div>
 		<ModalsRegisterModal />
 		<ModalsLoginModal />
-		<div class="w-16 h-16 m-12">
-			<button @click="removeFilters">No Filters</button>
-			<CategoryBox label="Mountains" iconName="ph:mountains" />
-			<CategoryBox label="GiorgioByMoroder" iconName="ph:mountains" />
-		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-const removeFilters = () => {
-		const query = useRoute().query;
-		const path = useRoute().fullPath;
-		console.log("query", query);
-		const urlWithoutQueryParams = path.split("?")[0];
 
-		return navigateTo(urlWithoutQueryParams);
-	};
 </script>
